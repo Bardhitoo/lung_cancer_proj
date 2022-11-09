1f5a806bcca62ff8e0490ba7e343a41c02cadb0b
Bardh R <bardhrushiti@gmail.com>
2022-11-08 20:36:11 -0500

    Fixed bug - takes labels into consideration

--- preprocessing_and_detection/saving_images.py
+++ preprocessing_and_detection/saving_images.py
@@ -11,12 +11,7 @@ import tensorflow as tf
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-
-DATA = "../data/"
-SCAN_PATH = DATA + "subset0"
-TRAIN_DATA_PATH = DATA + "split_dataset.pickle"
-EXPERIMENT_NAME = "subset0_3D_2mm"
-SAVE_IMG = DATA + f"{EXPERIMENT_NAME}_train_test_split/"
+from my_records import *
 
 if not os.path.exists(SAVE_IMG):
     os.mkdir(SAVE_IMG)
@@ -40,7 +35,7 @@ def normalizePlanes(npzarray):
     return npzarray
 
 
-def viz_bounding_box(image_slice, x, y, width=30):
+def viz_bounding_box(image_slice, x, y, width=40):
     # Display the image
     plt.figure()
     plt.imshow(image_slice)
@@ -52,6 +47,19 @@ def viz_bounding_box(image_slice, x, y, width=30):
     plt.show()
 
 
+def get_image_coords(s, row):
+    real_world_coords = row[1:].tolist()
+    origin = s.GetOrigin()
+    res = s.GetSpacing()
+
+    if res[2] != 0.625:
+        raiseValueError = 1
+        # raise ValueError("Inconsistent spacing")
+
+    x, y, z = [np.abs(real_world_coords[j] - origin[j]) / res[j] for j in range(len(real_world_coords))]
+    return x, y, z
+
+
 def main():
     with open(TRAIN_DATA_PATH, "rb") as input_file:
         train_test_split_dict = pickle.load(input_file)
@@ -61,14 +69,12 @@ def main():
     y_train = train_test_split_dict["y_train"]
     X_test = train_test_split_dict["X_test"]
     y_test = train_test_split_dict["y_test"]
-    for dataset, dataset_name in zip([X_train, X_test], ["train", "test"]):
+    for dataset_name, samples, labels in zip(["train", "test"], [X_train, X_test], [y_train, y_test]):
         df = pd.DataFrame(columns=dataset.columns)
         seen = {}
         counter = 0
 
-        for index, row in dataset.iterrows():
-            counter += 1
-            print(f"Processing: {dataset_name} - {counter} / {len(dataset) - 1}")
+        for index, row in samples.iterrows():
             sample_name = row["seriesuid"]
             s = sitk.ReadImage(os.path.join(SCAN_PATH, sample_name + ".mhd"))
 
@@ -78,6 +84,8 @@ def main():
             if s.GetSpacing()[2] <= 2:
                 continue
 
+            counter += 1
+            print(f"Processing: {dataset_name} - {counter}")
             if sample_name in seen:
                 seen[sample_name] = seen[sample_name] + 1
             else:
@@ -89,7 +97,7 @@ def main():
             x_, y_, z = get_image_coords(s, row)
 
             image_slice_m1 = image[int(z) - 1, :, :]
-            image_slice_0 = image[int(z)     , :, :]
+            image_slice_0 = image[int(z), :, :]
             image_slice_p1 = image[int(z) + 1, :, :]
 
             # viz_bounding_box(image_slice_m1, x_, y_, width=40)
@@ -104,20 +112,8 @@ def main():
             image_normalized = cv2.merge((image_m1_normalized, image_0_normalized, image_p1_normalized))
             cv2.imwrite(image_path, image_normalized * 255)
 
-            df.loc[len(df.index)] = [sample_name, x_, y_, z]
-        df.to_csv(f"processed_data/{dataset_name}_{EXPERIMENT_NAME}_transformed_coords.csv")
-
-def get_image_coords(s, row):
-    real_world_coords = row[1:].tolist()
-    origin = s.GetOrigin()
-    res = s.GetSpacing()
-
-    if res[2] != 0.625:
-        raiseValueError = 1
-        # raise ValueError("Inconsistent spacing")
-
-    x, y, z = [np.abs(real_world_coords[j] - origin[j]) / res[j] for j in range(len(real_world_coords))]
-    return x, y, z
+            df.loc[len(df.index)] = [sample_name, x_, y_, z, labels[index]]
+        df.to_csv(f"processed_data/{dataset_name}_{EXPERIMENT_NAME}_transformed_coords.csv", index=False)
 
 
 if __name__ == "__main__":
