2ecc3a7eb546f34788194d90fd231e07c96823bd
Bardh R <bardhrushiti@gmail.com>
2022-11-10 14:16:49 -0500

    Added to process subset1 of LUNA Dataset

--- preprocessing_and_detection/dataset_split.py
+++ preprocessing_and_detection/dataset_split.py
@@ -7,22 +7,25 @@ import pickle
 matplotlib.use('TkAgg')  # TODO: Experiment this on another env. of conda
 
 from sklearn.model_selection import train_test_split
-from my_records import *
+from preprocessing_and_detection.utils.my_utils import *
+
 
 def main():
-    def filter_no_downloads(df, path):
-        files = [file[:-4] for file in os.listdir(path) if file.endswith(".mhd")]
+    def filter_no_downloads(df, folder):
+        files = [file[:-4] for file in os.listdir(DATA + folder) if file.endswith(".mhd")]
         my_df = pd.DataFrame(columns=df.columns)
         for file in files:
-            if sum(df["seriesuid"] == file) > 0:
+            if df["seriesuid"].str.contains(file).any():
                 my_df = pd.concat([my_df, df[df["seriesuid"] == file]], axis=0)
-
+        my_df["path"] = DATA + folder + "/" + my_df['seriesuid'].astype(str) + ".mhd"
         return my_df
 
     total_candidates = pd.read_csv(CANDIDATE_FILE_PATH)
 
     # Filter out instances that I don't have on my local machine
-    my_candidates = filter_no_downloads(total_candidates, SCAN_PATH)
+    my_candidates_1 = filter_no_downloads(total_candidates, "subset0")
+    my_candidates_2 = filter_no_downloads(total_candidates, "subset1")
+    my_candidates = pd.concat([my_candidates_1, my_candidates_2], axis=0)
 
     #  TODO: Separate positive & negative cases by patient id
     # Dont want to have samples of patients split into train and test
@@ -36,10 +39,10 @@ def main():
     negative_cases = negative_cases.loc[neg_idx]
 
     my_candidates_df_idx = [list(positive_cases.index) + list(negative_cases.index)][0]
-    my_candidates_df = total_candidates.loc[my_candidates_df_idx]
+    my_candidates_df = my_candidates.loc[my_candidates_df_idx]
 
-    X = my_candidates_df.iloc[:, :-1]
-    y = my_candidates_df.iloc[:, -1]
+    X = my_candidates_df.loc[:, my_candidates_df.columns != "class"]
+    y = my_candidates_df["class"]
 
     # train test split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, random_state=42)
