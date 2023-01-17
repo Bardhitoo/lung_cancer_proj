0bd8bd210cb05c7d4b01265b3a8cfa8a2d738a8f
Bardh R <bardhrushiti@gmail.com>
2023-01-17 10:50:45 -0500

    Code developed on top of NoduleNet as part of the final approach

---
+++ 3d_nodule_viz.py
@@ -0,0 +1,81 @@
+import os
+
+try:
+    from skimage.measure import marching_cubes
+except ImportError:
+    # Old version compatible since marching_cubes replaced with marchin_cubes_lewiner in skimage 0.19.0
+    from skimage.measure import marching_cubes_lewiner as marching_cubes
+
+import numpy as np
+# import pylidc as pl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+viz3dbackends = ['matplotlib', 'mayavi']
+
+
+def find_nodules(scan_obj):
+    anns = scan_obj.annotations
+    masks = [ann.boolean_mask(pad=[(1, 1), (1, 1), (1, 1)]) for ann in anns if ann.calcification > 4]
+    return masks
+
+
+def nodule_3d_viz(nodule_mask, title, edgecolor='0.2', cmap='cool', step=1, figsize=(5, 5), backend='matplotlib'):
+    # Pad to cap the ends for masks that hit the edge.
+    if backend not in viz3dbackends:
+        raise ValueError("backend should be in %s." % viz3dbackends)
+
+    if backend == 'matplotlib':
+        if cmap not in plt.cm.cmap_d.keys():
+            raise ValueError("Invalid `cmap`. See `plt.cm.cmap_d.keys()`.")
+
+    mask = nodule_mask  # boolean_mask(pad=[(1,1), (1,1), (1,1)])
+    rij = 0.625  # self.scan.pixel_spacing
+    rk = 1  # self.scan.slice_thickness
+
+    if backend == 'matplotlib':
+        verts, faces, _, _ = marching_cubes(mask.astype(np.float), 0.5,
+                                            spacing=(rij, rij, rk),
+                                            step_size=step)
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(111, projection='3d')
+
+        t = np.linspace(0, 1, faces.shape[0])
+        mesh = Poly3DCollection(verts[faces],
+                                edgecolor=edgecolor,
+                                facecolors=plt.cm.cmap_d[cmap](t))
+        ax.add_collection3d(mesh)
+
+        ceil = max(mask.shape) * rij
+        ceil = int(np.round(ceil))
+
+        ax.set_xlim(0, ceil)
+        ax.set_xlabel('length (mm)')
+
+        ax.set_ylim(0, ceil)
+        ax.set_ylabel('length (mm)')
+
+        ax.set_zlim(0, ceil)
+        ax.set_zlabel('length (mm)')
+
+        plt.title(title)
+        plt.tight_layout()
+        plt.show()
+    elif backend == 'mayavi':
+        try:
+            from mayavi import mlab
+            sf = mlab.pipeline.scalar_field(mask.astype(np.float))
+            sf.spacing = [rij, rij, rk]
+            mlab.pipeline.iso_surface(sf, contours=[0.5])
+            mlab.show()
+        except ImportError:
+            print("Mayavi could not be imported. Is it installed?")
+
+
+if "__main__" == __name__:
+    nodule_path = r"C:\Users\bardh\Desktop\2021\RIT_CS\sem_2\independent_study\NoduleNet\BARDH\3d_contours"
+    for nodule_name in os.listdir(nodule_path):
+        title = "Patient ID: "+nodule_name[:-6]
+        nodule = np.load(os.path.join(nodule_path, nodule_name))
+
+        nodule_3d_viz(nodule, title, figsize=(10, 10))
---
+++ generate_contours.py
@@ -0,0 +1,74 @@
+import nrrd
+from utils.util import *
+from config import config
+if not os.path.exists("./BARDH/3d_contours"):
+    os.mkdir("./BARDH/3d_contours")
+
+
+def get_contours(img_res, nodule: pd.Series, pred: np.array):
+    D, H, W = img_res
+    z, y, x, d = nodule.coordZ, nodule.coordY, nodule.coordX, nodule.diameter_mm
+
+    z_start = max(0, int(np.floor(z - d / 2.)))
+    y_start = max(0, int(np.floor(y - d / 2.)))
+    x_start = max(0, int(np.floor(x - d / 2.)))
+    z_end = min(D, int(np.ceil(z + d / 2.)))
+    y_end = min(H, int(np.ceil(y + d / 2.)))
+    x_end = min(W, int(np.ceil(x + d / 2.)))
+
+    mask = pred[z_start: z_end, y_start: y_end, x_start: x_end].copy()
+
+    return mask
+
+
+def load_img(data_dir, path_to_img):
+    if path_to_img.startswith('LKDS'):
+        img = np.load(os.path.join(data_dir, '%s_clean.npy' % (path_to_img)))
+    else:
+        img, _ = nrrd.read(os.path.join(data_dir, '%s_clean.nrrd' % (path_to_img)))
+    img = img[np.newaxis, ...]
+
+    return img
+
+
+def load_mask(data_dir, filename):
+    mask, _ = nrrd.read(os.path.join(data_dir, '%s_mask.nrrd' % (filename)))
+
+    return mask
+
+
+data_dir = config['DATA_DIR']
+out_dir = os.path.join(config["out_dir"])
+load_dir = os.path.join(out_dir, 'res', str(200))
+
+rpns = pd.read_csv(load_dir + "\FROC\submission_rpn.csv")
+
+for uid in rpns.seriesuid.unique():
+    image = load_img(data_dir, uid)
+    normalized_img = normalize(image[0])
+
+    print("Generating Scan Animation... May take a moment.")
+    # generate_image_anim(image[0], save_path=f"my_figs/{uid}_anim.mp4")
+
+    pred = np.load(os.path.join(load_dir, uid + ".npy"))
+
+    gt = load_mask(data_dir, uid)
+
+    padded_img = pad2factor(normalized_img)
+
+    # get the row of the highest prediction
+    uid_nodules = rpns[(rpns.seriesuid == uid)]
+    uid_max_prob = uid_nodules[uid_nodules.probability >= 0.99]
+    print(f"\nContours of {uid}")
+    for idx, nodule in uid_max_prob.iterrows():
+        # bbox must be of [coordZ, coordY, coordX]
+        bbox = np.array([nodule.coordZ, nodule.coordY, nodule.coordZ], dtype=np.int16)
+
+        diamter = int(nodule.diameter_mm / 2) + 1
+        start = int(nodule.coordZ) - 1
+
+        nodule_contour = get_contours(image[0].shape, nodule, pred)
+
+        print(f"\tSaving contour {idx} of {uid}")
+        np.save(f"BARDH/3d_contours/{uid}_{idx}.npy", nodule_contour)
+    exit()
